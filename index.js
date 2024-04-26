75f1e08f49299f4657c140792480e8ba3cb885ec
sajidhasan-web <sajidhasan.web@gmail.com>
2024-04-27 01:20:15 +0600

    set collection in the database

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const spotsCollection = client.db("touristSpotsManagementDB").collection("spots")
+
+    
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
